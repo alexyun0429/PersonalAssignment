168082ebc3fc7862206db614098f8f347edba14c
alexyun0429 <jaewonyun@d-i89-198-10.student.eduroam.uq.edu.au>
2023-03-12 21:49:17 +1000

    adding smtp trial

---
+++ source/sendingEmail_SMTP/main.py
@@ -0,0 +1,17 @@
+import smtplib
+
+"""made new emails to try out the code"""
+my_gmail = "pythontrial0429@gmail.com"
+my_yahoomail = "pythontry0429@yahoo.com"
+password = "hxlbakzgjjcgssfa"
+
+try:
+    connection = smtplib.SMTP("smtp.gmail.com", 587)
+    connection.ehlo()
+    connection.starttls()
+    connection.login(user=my_gmail, password=password)
+    connection.sendmail(from_addr=my_gmail, to_addrs="seunghyun1118@gmail.com", msg="FUck YoU")
+    connection.close()
+except:
+    print
+    "something went wrong"
