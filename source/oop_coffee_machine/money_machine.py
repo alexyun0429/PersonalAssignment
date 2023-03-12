fe73f2a782f101962de8282ade0f6292eab2011a
alexyun0429 <jaewonyun@d-i89-198-10.student.eduroam.uq.edu.au>
2023-03-12 20:31:03 +1000

    upgraded coffee machine to OOP code

---
+++ source/oop_coffee_machine/money_machine.py
@@ -0,0 +1,43 @@
+class MoneyMachine:
+    CURRENCY = "$"
+
+    COIN_VALUES = {
+        "One Cent": 0.01,
+        "Two Cents":0.02,
+        "Five Cents": 0.05,
+        "Ten Cents": 0.1,
+        "Twenty Cents": 0.2,
+        "Fifty Cents": 0.5,
+        "One Dollar": 1,
+        "Two Dollars": 2,
+    }
+
+    def __init__(self):
+        self.profit = 0
+        self.money_received = 0
+
+    def report(self):
+        """Prints the current profit"""
+        print(f"Money: {self.CURRENCY}{self.profit}")
+
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins.")
+        for coin in self.COIN_VALUES:
+            self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+        return self.money_received
+
+    def make_payment(self, cost):
+        """Returns True when payment is accepted, or False if insufficient."""
+        self.process_coins()
+        if self.money_received >= cost:
+            change = round(self.money_received - cost, 2)
+            print(f"Here is {self.CURRENCY}{change} in change.")
+            self.profit += cost
+            self.money_received = 0
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            self.money_received = 0
+            return False
+
