1b06733925773287ddb7420a96bc359fb399bc18
JaeWon Yun <alexyun0429@gmail.com>
2023-07-18 01:51:21 +1000

    moved resume dir

--- source/my-resume/src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div>This is the footer</div>
-  )
-}
-
-export default Footer;
--- source/my-resume/src/pages/Portfolio/Portfolio.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Portfolio = () => {
-  return (
-    <div>This is the Portfolio</div>
-  )
-}
-
-export default Portfolio;
