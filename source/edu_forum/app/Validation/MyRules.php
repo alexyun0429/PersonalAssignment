33fc1c84da7006d9a3dcdc57307084ff301dfe5a
JaeWon Yun <alexyun0429@gmail.com>
2023-07-17 17:32:10 +1000

    took out eduforum

--- source/edu_forum/app/Validation/MyRules.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Validation;
-
-class MyRules
-{
-    public function check_password(string $str): bool
-    {
-        return preg_match('/[A-Z]/', $str) && preg_match('/\d/', $str) && preg_match('/[^a-zA-Z\d]/', $str);
-    }
-}
